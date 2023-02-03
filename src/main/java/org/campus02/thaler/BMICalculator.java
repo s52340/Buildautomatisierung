c3bfc20e0eb32716ce33606553ddee40a30ee5f9
bthaler <bianca.thaler@gmx.net>
2023-02-03 19:36:16 +0100

    added maven - surefire - plugin

--- src/main/java/org/campus02/thaler/BMICalculator.java
+++ src/main/java/org/campus02/thaler/BMICalculator.java
@@ -1,2 +1,42 @@
-package org.campus02.thaler;public class BMICalculator {
+package org.campus02.thaler;
+
+public class BMICalculator {
+    private double weightInKG;
+
+    private double heightInMeters;
+
+    public BMICalculator(double weightInKG, double heightInMeters) {
+        this.weightInKG = weightInKG;
+        this.heightInMeters = heightInMeters;
+    }
+
+    public double getWeightInKG() {
+        return weightInKG;
+    }
+
+    public double getHeightInMeters() {
+        return heightInMeters;
+    }
+
+    private double calculateBMI(){
+        return weightInKG / (heightInMeters * heightInMeters);
+    }
+
+    //bmiGrenzen
+    // <18,5 = underweight
+    // 18,5 - 25 normal
+    // 25 - 30 overweight
+    // >30 obese
+    public String result (){
+        double bmi = calculateBMI();
+        if (bmi < 18.5){
+            return "underweight";
+        } else if (bmi < 25) {
+            return "normal";
+        } else if (bmi < 30) {
+            return "overweight";
+        }else {
+            return "obvious";
+        }
+    }
 }
--- src/test/java/org/campus02/thaler/BMICalculatorTest.java
+++ src/test/java/org/campus02/thaler/BMICalculatorTest.java
@@ -1,4 +1,33 @@
+package org.campus02.thaler;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BMICalculatorTest {
-  
+
+    private BMICalculator bmiCalculator;
+
+    //alt + einf - stup methode
+    @BeforeEach
+    void setUp() {
+        bmiCalculator = new BMICalculator(180, 1.8);
+    }
+
+    @Test
+    @DisplayName("Konsturktor Tester")
+    void testConstruktor(){
+        double expectedWeight = 180;
+        Assertions.assertEquals(expectedWeight, bmiCalculator.getWeightInKG());
+        Assertions.assertEquals(1.8, bmiCalculator.getHeightInMeters());
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void result() {
+
+    }
 }
