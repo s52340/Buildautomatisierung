cfefcf62dd75ab7af8a7d6eed291545329ac650c
bthaler <bianca.thaler@gmx.net>
2023-02-03 20:04:14 +0100

    added test for result of bmi calculator

--- src/test/java/org/campus02/thaler/BMICalculatorTest.java
+++ src/test/java/org/campus02/thaler/BMICalculatorTest.java
@@ -29,6 +29,6 @@ class BMICalculatorTest {
     @org.junit.jupiter.api.Test
     void result() {
         String result = bmiCalculator.result();
-        Assertions.assertEquals("obese",result);
+        Assertions.assertEquals("obvious",result);
     }
 }
